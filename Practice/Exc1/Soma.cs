c96fcd3b5975a7b1c02cf76d452a032480c2e057
xxx <xxx@REVISION-PC>
2022-10-25 00:39:57 -0300

    Add project files.

---
+++ Practice/Exc1/Soma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Exc1
+{
+    internal class Soma
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("a");
+
+            Console.ReadKey();
+
+        }
+    }
+}
